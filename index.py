50eb6c9ca8995354afc59c84d36c1bfd23b9a5aa
yamamotocapsula <52672199+yamamotocapsula@users.noreply.github.com>
2019-07-31 10:56:16 -0300

    Update index.py

--- index.py
+++ index.py
@@ -6,22 +6,25 @@ import csv
 import sys
 import os
 
-
-
-
 with open("CEP.csv") as file:
 	reader = csv.DictReader(file)
 	cont = 0
 
 	file_new = open("log.txt", "w")
 	for row in reader:
+		
 		cep_line = str(row['cep'])
+		
 		r = requests.get("https://viacep.com.br/ws/" + cep_line + "/json/")
 		a = r.text
 		b = json.loads(a)
+		
 		final = str(cep_line) + ", " + str(b["bairro"] +  ", " + str(b["localidade"]) +  ", " + str(b["uf"])+ "\n")
 		print(final)
-#		time.sleep(2)
+		
+		# Adicionar aqui o limitador de tempo
+		# time.sleep(2)
+
 		file_new.writelines(final)
 	
 	file_new.close()
